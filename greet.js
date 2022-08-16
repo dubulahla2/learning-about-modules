e9f7cb5c51b4d53e9ceb12f6ed17d9c6eecc8188
dubulahla2 <100410536+dubulahla2@users.noreply.github.com>
2022-08-16 13:05:17 -0700

    First-Module

---
+++ greet.js
@@ -0,0 +1,3 @@
+export default function greet(neighboursName){
+  return "Hello, " + neighboursName
+}
---
+++ greet_xola.js
@@ -0,0 +1,6 @@
+import chalk from 'figlet';
+//import the greet module that is in the current folder
+import greet from './greet.js'
+
+const styledMessage = chalk.bgGreen.black(greet('Xola'));
+console.log(styledMessage)
